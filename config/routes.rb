8ae36cc620ed9a8a8d4d24bb95255273c35b7923
EC2 Default User <ec2-user@ip-172-31-50-231.ec2.internal>
2021-05-03 12:58:44 +0000

    [comment nice] bookers2-debug2

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resource :book_comments, only: [:create, :destroy]
+  end
   
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
   get 'home/about' => 'homes#about'
   
 end
