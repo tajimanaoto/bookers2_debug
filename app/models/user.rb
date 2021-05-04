75ccdcc3a06ef4f8a07ef90cf582c52acc261f36
EC2 Default User <ec2-user@ip-172-31-50-231.ec2.internal>
2021-05-04 10:30:21 +0000

    [follower finish]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,27 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+ 
   attachment :profile_image
 
   validates :name, uniqueness: true, length: {maximum: 20, minimum: 2}
   validates :introduction,  uniqueness: true,  length: { maximum: 50 }
   
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
